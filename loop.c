c79a5c51f3904b190db3c73b04c52deb420326ae
Safiyya03 <syedasafiyya08@gmail.com>
2026-08-09 21:49:30 +0530

    Add loop to print numbers from 0 to 9
    Implement a loop that prints numbers from 0 to 9.

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int i;
+
+    /* The loop goes while i < 10, and i increases by one every loop*/
+
+    for ( i = 0; i < 10; i++ ) {
+        printf( "%d\n", i );
+    }
+}
